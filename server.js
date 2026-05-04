a811f893629cf9d860bbba5af71b034c40e03c72
Kaka-Sharma <pallavsharma407@gmail.com>
2026-05-04 13:42:29 +0530

    fix helmet

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ app.use((req, res, next) => {
 //   }),
 // );
 console.log(process.env.CLIENT_URL)
-// app.use(helmet())
+app.use(helmet())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
